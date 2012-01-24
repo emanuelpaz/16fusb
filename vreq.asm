1d4e50b0b7c5321579740da0aceb19f3cbd3532f
efspaz <efspaz@gmail.com>
2012-01-24 01:59:41 +0000

    Initial commit.

---
+++ vreq.asm
@@ -0,0 +1,43 @@
+;**********************************************************************
+;   16FUSB - USB 1.1 implemetation for PIC16F628/648                  *
+;                                                                     *
+;   Copyright (C) 2011  Emanuel Paz <efspaz@gmail.com>                *
+;                                                                     *
+;   This program is free software; you can redistribute it and/or     *
+;   modify it under the terms of the GNU General Public License as    *
+;   published by the Free Software Foundation; either version 2 of    *
+;   the License, or (at your option) any later version.               *
+;                                                                     *
+;**********************************************************************
+;                                                                     *
+;    Filename:        vreq.asm                                        *
+;    Date:                                                            *
+;    Author:          Emanuel Paz                                     *
+;                                                                     *
+;**********************************************************************
+;                                                                     *
+;    Files required: header.inc, cvar.inc                             *
+;                                                                     *
+;**********************************************************************
+;                                                                     *
+;    Notes: All custom (vendor) requests goes here.                   *
+;                                                                     *
+;**********************************************************************	
+
+
+    include     "header.inc"
+    include     "cvar.inc"
+	
+    global      VendorRequest
+
+    extern      SetFreeAndReturn, SetReadyAndReturn, ComposeNullAndReturn
+    extern      PreInitTXBuffer, DoCrc, InsertStuff
+
+VENDOR_REQUEST  CODE
+
+VendorRequest:
+
+    ;Custom code goes here
+
+
+	END
