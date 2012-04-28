04cb5771e3a697875a9d3917c1e1e3da8e969d3d
efspaz@gmail.com <efspaz@gmail.com@722d11a5-5cdf-a247-678d-b908f2c9397d>
2012-04-28 18:05:24 +0000

    - Decode process moved from ISR to MainLoop (http://code.google.com/p/16fusb/issues/detail?id=1).   Reason: Some faster ports weren't recognizing the device. This happened because while the decoding           was in process, the device couldn't answer any host request.
    - Project structure changed in accordance with the issue 2 (http://code.google.com/p/16fusb/issues/detail?id=2).
    
    - This revision also include RAM reorganization, 'Interface API' elimination and code enhancement.

--- trunk/16fusb/isr.asm
+++ trunk/16fusb/isr.asm
@@ -1,7 +1,7 @@
 ;**********************************************************************
 ;   16FUSB - USB 1.1 implemetation for PIC16F628/628A                 *
 ;                                                                     *
-;   Copyright (C) 2011  Emanuel Paz <efspaz@gmail.com>                *
+;   Copyright (C) 2011-2012  Emanuel Paz <efspaz@gmail.com>           *
 ;                                                                     *
 ;   This program is free software; you can redistribute it and/or     *
 ;   modify it under the terms of the GNU General Public License as    *
@@ -16,10 +16,6 @@
 ;                                                                     *
 ;**********************************************************************
 ;                                                                     *
-;    Files required: header.inc                                       *
-;                                                                     *
-;**********************************************************************
-;                                                                     *
 ;    Notes: Unless you know exactly what you doing, do not edit       *
 ;           this file. Timing is very strict here! This code is       *
 ;           the interrupt service routine that treats every           *
@@ -29,26 +25,65 @@
 ;**********************************************************************
 
 
-    include     "header.inc"
-    
+    #include    "def.inc"
+
+    ;From LOCAL_OVERLAY of main.asm -----------------------------------
+    extern      RXINPUT_LEN, RXINPUT_BUFFER, DECODING_PID
+
+
+ISR_SHARED_INTERFACE    UDATA_SHR
+
+ACTION_FLAG             RES     D'1'    ;What main loop must do
+ADDITIONAL_BITS         RES     D'1'    ;Number of additional bits(stuffing bits) to send
+FRAME_NUMBER            RES     D'1'    ;Frame number of a transaction with data larger than 8 bytes
+PENDING_BYTES           RES     D'1'    ;A bit (0) that signals if there are pending bytes to send
+                                        ;Used in transactions with data larger than 8 bytes
+DEVICE_ADDRESS          RES     D'1'    ;Current device address
+NEW_DEVICE_ADDRESS      RES     D'1'    ;Address designated by host
+TX_LEN                  RES     D'1'    ;Number of bytes to send in TX_BUFFER
+
+    global      ACTION_FLAG, ADDITIONAL_BITS, FRAME_NUMBER, PENDING_BYTES
+    global      DEVICE_ADDRESS, NEW_DEVICE_ADDRESS, TX_LEN
+
+
+ISR_VARIABLES           UDATA   0x20
+
+W_TMP                   RES     D'1'    ;File to save W
+STATUS_TMP              RES     D'1'    ;File to save STATUS
+FSR_TMP                 RES     D'1'    ;FIle to save FSR
+TMP                     RES     D'1'    ;Temporary file
+COUNT                   RES     D'1'    ;Counter file level one
+SEEK                    RES     D'1'    ;Index file level one
+SEEK2                   RES     D'1'    ;Index file level two
+LAST_TOKEN_PID          RES     D'1'    ;The last Token PID received
+RX_LEN                  RES     D'1'    ;Number of bytes received in RX_BUFFER
+RX_BUFFER               RES     D'13'   ;NRZI data received from host, with bit stuffing
+TX_BUFFER               RES     D'13'   ;Tranmission Buffer. Contains data (not coded) to send
+
+    global      TX_BUFFER
+
+
+
 INTERRUPT_VECTOR    CODE    0x0004
 ; -------------- Sync (start) --------------
     ;Save context first
     movwf   W_TMP
     swapf   STATUS,W
     movwf   STATUS_TMP
-    bcf     PORTB,0                     ;prepare latch
+
+    ;Select BANK 0
+    bcf     STATUS,RP0
+    bcf     STATUS,RP1
 
 WaitForJ:
-    nop
     btfsc   PORTB,0
     goto    WaitForJ
 WaitForK:
     btfss   PORTB,0
     goto    WaitForK    
-    nop
+    movf    FSR,W
 FoundK:
-    nop
+    movwf   FSR_TMP
     btfss   PORTB,0
     goto    WaitForK    
     
@@ -134,10 +169,10 @@ TokenPack:
 
 HandleIn:
     movlw   AF_TX_BUFF_READY
-    subwf   ACTION_FLAG,W           ;Check ACTION_FLAG
+    subwf   ACTION_FLAG,W           ;Checks ACTION_FLAG
     btfsc   STATUS,Z
     goto    $+D'4'                  ;Jump if answer is ready
-    movlw   NAKPID                  ;Put NAK in TX_BUFFER
+    movlw   NAKPID                  ;Put NAK to send
     call    SendHandshake           ;Send NAK
     goto    ReturnFromISR           ;And leave interrupt service
 
@@ -146,11 +181,14 @@ HandleIn:
     movwf   DEVICE_ADDRESS          ;Renew device address
     movlw   AF_PROC_SETUP
     btfsc   PENDING_BYTES,0
-    goto    $+D'3'                  ;Jump if theres pending bytes
+    goto    HI_AdjustFrame          ;Jump if theres pending bytes
 
     movlw   AF_FREE                 ;If there are no pending bytes, set it free
-    clrf    FRAME_NUMBER
+    goto    $+D'2'
 
+HI_AdjustFrame:
+    incf    FRAME_NUMBER,F          ;Increment frame if there are pending bytes
+    
     movwf   ACTION_FLAG             ;Set ACTION_FLAG
     goto    ReturnFromISR
 
@@ -164,6 +202,8 @@ HandleSetupOut:
     
 ReturnFromISR:
     ;Restore Context
+    movf    FSR_TMP,W
+    movwf   FSR
     swapf   STATUS_TMP,W
     movwf   STATUS
     swapf   W_TMP,F
@@ -183,152 +223,70 @@ DataPack:
     subwf   FSR,W
     movwf   RX_LEN
 
-    movlw   AF_TX_BUFF_READY
-    subwf   ACTION_FLAG,W
-    btfss   STATUS,Z
-    goto    $+D'3'
     movlw   AF_FREE
-    movwf   ACTION_FLAG
-
-    movlw   AF_FREE
-    subwf   ACTION_FLAG,W           ;Check ACTION_FLAG
+    subwf   ACTION_FLAG,W           ;Checks ACTION_FLAG
     btfsc   STATUS,Z
     goto    $+D'4'
 
     movlw   NAKPID
-    call    SendHandshake           ;If ACTION_FLAG is NOT FREE, send NAK
-    goto    ReturnFromISR
+    call    SendHandshake           ;If ACTION_FLAG is not free, send NAK
+    goto    ReturnFromISR           ;And return
     
     movlw   ACKPID                  ;If it's free...
     call    SendHandshake           ;Send ACK
     
-    ;Adjust ACTION_FLAG
-    movlw   AF_PROC_OUT
-    btfss   LAST_TOKEN_PID,2        ;Data from OUT?
-    movlw   AF_PROC_SETUP           ;no, it's from SETUP
-    movwf   ACTION_FLAG
-
-;Decode RX_BUFFER to RXDATA_BUFFER
-DP_PrepareDecoding:
+    ;if it's a zero length packet, just return.
     movf    RX_LEN,W
-    movwf   COUNT
-   
-    movlw   RX_BUFFER+1             ;Ignore Token PID
-    movwf   SEEK1                   ;Source
-    clrf    RXDATA_LEN
-    movlw   RXDATA_BUFFER
-    movwf   SEEK2                   ;Destiny
-
-    movlw   0x01
-    movwf   GEN2                    ;Roles as "last nrzi bit"
-
-    movlw   0x06                    ;If DATA1
-    btfsc   RX_BUFFER,6   
-    movlw   0x04                    ;If DATA0
-    movwf   NCHANGE_COUNT
-   
-    movlw   0x08
-    movwf   COUNT3                  ;COUNT3 controls bits in destiny
-
-DP_CopyLoop:                        ;Controlled by COUNT
-    movf    SEEK1,W
-    movwf   FSR
-    movf    INDF,W
-    movwf   TMP                     ;Put in TMP the byte to decode
-   
-    movf    SEEK2,W
-    movwf   FSR
-   
-    movlw   0x08                    ;Decode 8 bits
-    movwf   COUNT2
-
-DP_CP_DecodeBit:                    ;Controlled by COUNT2
-    movlw   0x01
-    andwf   TMP,W
-    movwf   GEN1       
-    xorwf   GEN2,F
-    comf    GEN2,F
-
-    btfss   GEN2,0
-    goto    $+D'4'
-    decfsz  NCHANGE_COUNT,F
-    goto    $+D'4'
-    goto    DP_RemoveBitStuff
-
-    movlw   0x06
-    movwf   NCHANGE_COUNT   
-
-    rrf     GEN2,F
-    rrf     INDF,F
-    movf    GEN1,W
-    movwf   GEN2
-
-DP_CP_DB_VerifyDestEnd:
-    decfsz  COUNT3,F
-    goto    DP_CP_DB_VerifyEnd
-
-    incf    RXDATA_LEN,F
-    movf    RXDATA_LEN,W
-    subwf   RX_LEN,W
-    sublw   0x03                    ;Skip PID and CRC16 on decoding (3 bytes) 
+    sublw   0x03
     btfsc   STATUS,Z
-    goto    DP_CP_End
-
-    incf    SEEK2,F
-    movf    SEEK2,W
-    movwf   FSR
-    movlw   0x08
-    movwf   COUNT3
-
-DP_CP_DB_VerifyEnd:
-    rrf     TMP,F
-    decfsz  COUNT2,F
-    goto    DP_CP_DecodeBit
-DP_CP_DB_End:
-    incf    SEEK1,F
-    decfsz  COUNT,F
-    goto    DP_CopyLoop
-DP_CP_End:
-    clrf    FRAME_NUMBER
-    movlw   DATA0PID
-    movwf   TX_BUFFER
-    
-    ;Put setup's wLenght in TOTAL_LEN
-    movf    RXDATA_BUFFER+6,W
-    movwf   TOTAL_LEN
     goto    ReturnFromISR
 
-DP_RemoveBitStuff:
-    movlw   0x06
-    movwf   NCHANGE_COUNT
-   
-    rrf     GEN2,F
-    rrf     INDF,F
 
-    decfsz  COUNT3,F
-    goto    $+D'12'
-    incf    RXDATA_LEN,F
-    movf    RXDATA_LEN,W
-    subwf   RX_LEN,W
-    sublw   0x03                    ;Skip PID and CRC16 on decoding (3 bytes)
-    btfsc   STATUS,Z
-    goto    DP_CP_End
-    incf    SEEK2,F
+DP_PrepareDataToDecoding:
+    movlw   AF_DECODE_DATA          ;Tells MainLoop to decode data
+    movwf   ACTION_FLAG
+
+    movf    LAST_TOKEN_PID,W
+    movwf   DECODING_PID
+
+    decf    RX_LEN,W
+    movwf   RXINPUT_LEN 
+    movwf   COUNT
+
+    movlw   RX_BUFFER+1             ;Ignore Token PID
+    movwf   SEEK                    ;Source
+
+    movlw   RXINPUT_BUFFER
+    movwf   SEEK2                   ;Destiny
+
+;Copy RX_BUFFER to RXINPUT_BUFFER
+DP_PDTD_CopyBuffer:
+    ;source
+    movf    SEEK,W
+    movwf   FSR
+    movf    INDF,W
+    movwf   TMP
+
+    ;destiny
     movf    SEEK2,W
     movwf   FSR
-    movlw   0x08
-    movwf   COUNT3
+    movf    TMP,W
+    movwf   INDF
 
-    rrf     TMP,F
-    movlw   0x01
-    andwf   TMP,W
-    movwf   GEN2
+    incf    SEEK,F
+    incf    SEEK2,F
+    decfsz  COUNT,F
+    goto    DP_PDTD_CopyBuffer
 
-    decfsz  COUNT2,F
-    goto    DP_CP_DB_VerifyEnd
-    goto    DP_CP_DB_End
+    goto    ReturnFromISR
 
 SendTXBuffer:
+    ;Prepare latch
+    bsf     PORTB,1                 ;RB0 will by 0 due to R-M-W (we're in EOP)
+
+    movlw   0x03
+    addwf   TX_LEN,F                ;TX_LEN += PID + CRC16 (3 bytes)
+
     ;Set RB0/RB1 Output
     bsf     STATUS,RP0
     movlw   0xFC
@@ -359,7 +317,7 @@ SendTXBuffer:
     movwf   INTCON
     movlw   0x03
     nop
-    nop                             ;Here, 1 bit time elapsed. TRM0 = 1.
+    nop                             ;Here, 1 bit time elapsed. TRM0 += 1.
 
     ;These two 'nops' makes T0IF verification at the last cycle of bit time,
     ;so we can jump to the EOP with only one excessive instruction cycle (166ns)
@@ -453,11 +411,9 @@ GenEop_02:
 
 SendHandshake:
     movwf   TMP                     ;Save W (PID to send) in TMP
-    movlw   TX_BUFFER
-    movwf   FSR
-    
-    movf    TMP,W
-    movwf   INDF
+
+    ;Prepare latch
+    bsf     PORTB,1                 ;RB0 will by 0 due to R-M-W (we're in EOP)
 
     ;Set RB0/RB1 Output
     bsf     STATUS,RP0
@@ -467,8 +423,8 @@ SendHandshake:
 
     movlw   0x07
     movwf   COUNT
-    movlw   0x03
-    
+
+    movlw   0x03    
 SH_SyncLoop:                        ;Send Sync    
     xorwf   PORTB,F
     decfsz  COUNT,F
@@ -476,42 +432,42 @@ SH_SyncLoop:                        ;Send Sync
     call    WaitOneBitTime
 
     ;Send HandShake PID
-    btfss   INDF,0
+    btfss   TMP,0
     xorwf   PORTB,F
     nop
     nop
     
-    btfss   INDF,1
+    btfss   TMP,1
     xorwf   PORTB,F
     nop
     nop
 
-    btfss   INDF,2
+    btfss   TMP,2
     xorwf   PORTB,F
     nop
     nop
     
-    btfss   INDF,3
+    btfss   TMP,3
     xorwf   PORTB,F
     nop    
     nop
 
-    btfss   INDF,4
+    btfss   TMP,4
     xorwf   PORTB,F
     nop
     nop
 
-    btfss   INDF,5
+    btfss   TMP,5
     xorwf   PORTB,F
     nop
     nop
 
-    btfss   INDF,6
+    btfss   TMP,6
     xorwf   PORTB,F
     nop
     nop
 
-    btfss   INDF,7
+    btfss   TMP,7
     xorwf   PORTB,F
     nop
     nop
@@ -529,7 +485,7 @@ SH_SyncLoop:                        ;Send Sync
     bsf     STATUS,RP0
     movlw   0x03
     iorwf   TRISB,F
-    bcf     STATUS,RP0              ;Back to bank 0    
+    bcf     STATUS,RP0
     return
 
 WaitOneBitTime:
