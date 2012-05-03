f8bf687ba13973fde7656bb44d88b71855d13ff6
efspaz@gmail.com <efspaz@gmail.com@722d11a5-5cdf-a247-678d-b908f2c9397d>
2012-05-03 00:44:39 +0000

    Select of Bank 0 on reset check (MainLoop).

--- branches/directio/16fusb/main.asm
+++ branches/directio/16fusb/main.asm
@@ -162,7 +162,7 @@ DecodeData:
     movwf   COUNT
    
     movlw   RXINPUT_BUFFER
-    movwf   SEEK                   ;Source
+    movwf   SEEK                    ;Source
     clrf    RXDATA_LEN
     movlw   RXDATA_BUFFER
     movwf   SEEK2                   ;Destiny
@@ -381,6 +381,10 @@ SetFreeAndReturn:
     return
         
 CheckReset:
+    ;Select BANK 0
+    bcf     STATUS,RP0
+    bcf     STATUS,RP1
+
     movlw   0x3                     ;For 15 instructions (2.5us)
     movwf   COUNT
 CR_Loop:
